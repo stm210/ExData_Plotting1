7da6aab20a98fec459ce3d575673dd95fc073bec
stm210 <stephanie.magnuson@gmail.com>
2015-05-10 14:49:36 -0500

    plot2 R code

--- plot2.R
+++ plot2.R
@@ -7,11 +7,9 @@ names(hpc_data)[1] <- "record_date"
 names(hpc_data)[2] <- "record_time"
 
 hpc_data$record_date <- as.Date(as.character(hpc_data$record_date), '%d/%m/%Y')
+hpc_data$date_time <- as.POSIXct(paste(hpc_data$record_date, hpc_data$record_time), format="%Y-%m-%d %H:%M:%S")
 
 hpc_data_dates <- subset(hpc_data, record_date >= '2007-02-01' & record_date <= '2007-02-02')
-
-##check this- setting to 5/8
-hpc_data_dates$record_time <- strptime(hpc_data_dates$record_time, format = "%H:%M:%S")
 
 hpc_data_dates$Global_active_power <- as.character(hpc_data_dates$Global_active_power)
 
@@ -20,12 +18,5 @@ hpc_data_dates <- hpc_data_dates[complete.cases(hpc_data_dates$Global_active_pow
 
 hpc_data_dates$Global_active_power <- as.numeric(hpc_data_dates$Global_active_power)
 
-hpc_data_dates$day_of_week <- weekdays(hpc_data_dates$record_date)
-
-plot2 <- plot(x = hpc_data_dates$day_of_week, y = hpc_data_dates$Global_active_power, type = "l" )
-plotx <- plot(hpc_data_dates$record_date,hpc_data_dates$Global_active_power, type = "l")
+plot2 <- with(hpc_data_dates, plot(date_time,Global_active_power, type = "l"))
 png("plot2.png", width= 480, height = 480, units = "px")
-              
-## , type = "l", ylab = "Global Active Power (kilowatts)",
-##  xlab = "Day of Week", main = "Global Active Power by Day",
-##  )
